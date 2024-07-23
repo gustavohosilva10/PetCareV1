14a93487869fc7aa0258df4fb61222b09d587faa
gustavobamaq <109670514+gustavobamaq@users.noreply.github.com>
2024-07-23 20:16:17 -0300

    adição de mascara e textos

---
+++ src/utils/text.js
@@ -0,0 +1,59 @@
+export const txtTittle = 'Bem-vindo(a) á PetCare!';
+export const txtSubtittle = 'O app de identificação do seu pet';
+export const txtRedefinitionPassword = 'Esqueceu a senha?';
+export const txtRedefinition = 'Redefinir';
+export const txtAccountQuestion = 'Não possui conta?';
+export const txtRegister = 'Cadastrar';
+export const txtLogin = 'Entrar';
+export const txtBack = 'Voltar';
+export const txtSend = 'Enviar';
+export const txtCreateAccount = 'Criar uma conta';
+export const txtInfoCreateAccount = 'É rápido, precisamos apenas de algumas informações';
+export const txtBackLogin = 'Já possui uma conta?';
+export const txtVerifyMail = 'Verifique seu e-mail';
+export const txtVerifyInfo = 'O link para redefinição foi enviado!';
+
+
+export const txtQuestionPassword = 'Precisa redefinir a sua senha?';
+export const txtInfoPassword = 'Informe o seu endereço de e-mail e será enviado um link para criar a nova senha.';
+
+export const txtNotValidForm = 'Todos os campos são obrigatórios!';
+
+export const txtEmail = 'E-mail';
+export const txtPassword = 'Senha';
+export const txtName = 'Nome completo';
+export const txtCellphone = 'Celular';
+export const txtDocument = 'Cpf';
+export const txtBirthDate = 'Data de nascimento';
+
+
+export const txtTittleOne = 'Tenha todos os dados do seu pet na palma da mão';
+export const txtSubTittleOne = 'Cadastre as informações do seu pet, sempre que necessário consulte o app';
+
+export const txtTittleTwo = 'Identificação fácil e rápida';
+export const txtSubTittleTwo = 'Com a Meu Pet a identificação do seu pet é feita de forma super prática. Use o Qr code para identificação do seu pet nos estabelecimentos.';
+
+export const txtTittleThree = 'Cartão de vacinação';
+export const txtSubTittleThree = 'Não precisa mais passar por problemas com o cartão físico de vacinação! Use o cartão virtual e lembre-se de sempre mante-lo atualizado!';
+
+
+export const txtWelcome = 'Olá, ';
+export const txtMyPets = 'Meus pets';
+export const txtNonePet = 'Cadastre o seu pet!';
+export const txtCategoys = 'Categorias';
+
+export const txtTittleCardVaccine = 'Cartão de vacinas';
+export const txtSubTittleCardVaccine = 'Registre e acompanhe as vacinas';
+export const txtTittleCardMedicament = 'Medicamentos';
+export const txtSubiTittleCardMedicament = 'Registre os medicamentos do seu pet';
+export const txtTittleCardIdentification = 'Identificação';
+export const txtSubiTittleCardIdentification = 'Econtre e monitore o seu pet';
+
+export const txtCreatedAccount = 'Criação da conta:';
+export const txtMyInfo = 'Meus dados';
+export const txtUpdateInfo = 'Edite as informações do seu perfil';
+export const txtMyProfile = 'Meu perfil';
+export const txtMyProfileInfo = 'Edite as informações do seu perfil sempre que necessario';
+export const txtUpdateProfileTitle = 'Editar perfil';
+
+export const txtSave = 'Salvar';
